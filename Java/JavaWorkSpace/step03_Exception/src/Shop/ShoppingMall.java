62ff6382b3f0556dadb15408e7032def29024a3d
Iam-YJ <59785752+Iam-YJ@users.noreply.github.com>
2020-08-10 19:32:49 +0900

    .

--- Java/JavaWorkSpace/step03_Exception/src/Shop/ShoppingMall.java
+++ Java/JavaWorkSpace/step03_Exception/src/Shop/ShoppingMall.java
@@ -9,7 +9,10 @@ public class ShoppingMall {
 		if (age < 18) {
 			// 예외를 발생시킨다
 			ExceptionExam ee = new ExceptionExam("애들은 가라");
-			throw ee; // 에러 발생하면 -> ExceptionExam으로 던져!
+			throw ee;
+		
+			//멈춰
+			// 에러 발생하면 -> ExceptionExam으로 던져!
 			// throw new ExceptionExam("애들은 가라"); - 한문장 축약
 
 		} else {
